0d5e1c00c3c1e17bcab8e174866aedb495f65bd4
nFunk13 <nick.d.funk@gmail.com>
2025-12-22 12:06:07 -0600

    Description Text
     * Adding text that lets the player understand what they are doing

--- test/paddle/Main_Menu.lua
+++ test/paddle/Main_Menu.lua
@@ -6,7 +6,13 @@ end
 
 function Draw_Menu()
     cls(5)
-    print("MAIN MENU", 63, 63, 7)
+    print("\^t\^wlava breakout", 13, 5, 8)
+
+    print("gain points by:", 35, 30, 11)
+    print("breaking bricks", 35, 40, 11)
+    print("going up", 49, 50, 11)
+    
+    print("press z to start", 30, 120, 14)
 end
 
 function Update_Game()
